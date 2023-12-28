850b47a3673be9e884af252756fee99254ef5cca
mibu119 <horang296@gmail.com>
2023-12-28 14:28:12 +0900

    feat: Text 컴포넌트 구현

---
+++ src/components/common/Text/index.tsx
@@ -0,0 +1,46 @@
+import { VariantProps } from 'class-variance-authority';
+import { ComponentProps, ReactNode } from 'react';
+
+import { cn } from '~/utils/cn';
+
+import { TextVariants } from './Text.variants';
+
+export interface TextProps
+  extends VariantProps<typeof TextVariants>,
+    ComponentProps<'div'> {
+  children: ReactNode;
+  color?:
+    | 'primary'
+    | 'green'
+    | 'red'
+    | 'lightGreen'
+    | 'white'
+    | 'black'
+    | 'lightGray'
+    | 'gray';
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  strong?: boolean;
+  decoration?: 'none' | 'underline' | 'lineThrough' | 'overline';
+}
+
+const Text = ({
+  children,
+  className,
+  color,
+  size,
+  strong,
+  decoration,
+}: TextProps) => {
+  return (
+    <div
+      className={cn(
+        TextVariants({ color, size, strong, decoration }),
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Text;
