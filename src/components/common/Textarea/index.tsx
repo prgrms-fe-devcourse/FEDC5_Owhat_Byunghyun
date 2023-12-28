e41f47a6080f3bc4f1c1d511e4d8c53e6ef248fc
heeeeera <heeeeera5487@gmail.com>
2023-12-28 23:27:42 +0900

    feat: Textarea 컴포넌트 구현(#9)

---
+++ src/components/common/Textarea/index.tsx
@@ -0,0 +1,50 @@
+import { ComponentProps, useRef } from 'react';
+
+import { cn } from '~/utils/cn';
+
+interface TextareaProps extends ComponentProps<'textarea'> {
+  size?: 'sm' | 'lg';
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+}
+
+const Textarea = ({
+  size = 'sm',
+  placeholder = '',
+  required = false,
+  disabled = false,
+  className,
+  ...props
+}: TextareaProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const resizeTextarea = () => {
+    const textarea = textareaRef.current;
+
+    if (!textarea) return;
+
+    if (size === 'sm' && Number(textarea.scrollHeight) > 145) return;
+
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  };
+
+  return (
+    <textarea
+      placeholder={placeholder}
+      required={required}
+      disabled={disabled}
+      ref={textareaRef}
+      onInput={resizeTextarea}
+      rows={1}
+      className={cn(
+        'box-border w-full resize-none px-3 py-1.5 outline-none',
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+export default Textarea;
