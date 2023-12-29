67f3676b6c898e4aa47cc5626912bdfb0f02d8aa
mibu119 <horang296@gmail.com>
2023-12-29 18:19:52 +0900

    fix: input.variants default 통일, 기본 classname 순서 정렬

--- src/components/common/Input/Input.variants.ts
+++ src/components/common/Input/Input.variants.ts
@@ -1,23 +1,21 @@
 import { cva } from 'class-variance-authority';
 
 export const InputVariants = cva(
-  `rounded-[8px] px-2 py-1 text-black placeholder:text-sm border border-gray-300 focus:outline-none focus:border-primary`,
+  `rounded-lg border border-gray-300 px-2 py-1 text-black placeholder:text-sm focus:border-primary focus:outline-none`,
   {
     variants: {
       isBorderNone: {
-        default: false,
         true: 'border-none',
         false: 'border',
       },
       isBottomBorderOnly: {
-        default: false,
         true: 'rounded-none border-x-0 border-t-0',
         false: '',
       },
     },
     defaultVariants: {
-      isBorderNone: 'default',
-      isBottomBorderOnly: 'default',
+      isBorderNone: false,
+      isBottomBorderOnly: false,
     },
   },
 );
