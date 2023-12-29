fa90e256ea4679b8b7da690faa57cca59ca86175
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2023-12-29 11:03:59 +0900

    feat: Icon 컴포넌트 추가 (#16)
    * feat: Icon 컴포넌트 추가
    
    * refactor: 절대 경로 사용
    
    * refactor: 불필요한 id 제거 삭제

---
+++ src/stories/Icon.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Icon from '~/components/common/Icon';
+import { IconName } from '~/components/common/Icon/type';
+
+const meta: Meta<typeof Icon> = {
+  title: 'components/Icon',
+  component: Icon,
+};
+export default meta;
+
+type Story = StoryObj<typeof Icon>;
+
+const Icons: Array<IconName> = [
+  'account-circle',
+  'add-circle',
+  'add-photo',
+  'arrow-back',
+  'cancel',
+  'close',
+  'edit',
+  'favorite',
+  'forum',
+  'home',
+  'more-vert',
+  'notifications',
+  'search',
+  'send',
+  'sms',
+];
+
+export const Default: Story = {
+  args: {
+    size: 24,
+    id: 'account-circle',
+  },
+  argTypes: {
+    size: {
+      control: 'range',
+      min: 1,
+      max: 100,
+    },
+    id: {
+      control: 'radio',
+      options: Icons,
+    },
+  },
+};
