2c07094a77d4bd2ad2a0f2b187f536f74913f42b
mibu119 <horang296@gmail.com>
2023-12-29 12:43:51 +0900

    feat: input 컴포넌트 UI 스토리 생성

---
+++ src/components/common/Input/Input.variants.ts
@@ -0,0 +1,32 @@
+import { cva } from 'class-variance-authority';
+
+export const InputVariants = cva(``, {
+  variants: {
+    borderColor: {
+      default: 'border border-gray-300',
+      white: 'border border-white',
+      primary: 'border border-primary',
+    },
+    outlineColor: {
+      default: 'focus:outline-none focus:border-primary',
+      lightGray: 'focus:outline-none focus:border-gray-300',
+      none: 'focus:outline-none focus:border-transparent',
+    },
+    isBorderNone: {
+      default: false,
+      true: 'rounded-none border-x-0 border-y-0 ',
+      false: 'border',
+    },
+    isBottomBorderOnly: {
+      default: false,
+      true: 'rounded-none border-x-0 border-t-0',
+      false: '',
+    },
+  },
+  defaultVariants: {
+    borderColor: 'default',
+    outlineColor: 'default',
+    isBorderNone: 'default',
+    isBottomBorderOnly: 'default',
+  },
+});
