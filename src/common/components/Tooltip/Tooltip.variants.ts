990df703da602699dc8d58c33654f65ec33b7c96
mibu119 <horang296@gmail.com>
2023-12-31 16:06:44 +0900

    refactor: classname 순서 정렬

--- src/common/components/Tooltip/Tooltip.variants.ts
+++ src/common/components/Tooltip/Tooltip.variants.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 export const tooltipVariants = cva(
-  `duration-500 pointer-events-none absolute z-10 rounded-lg border border-gray-300 bg-white px-3 py-2 text-center text-sm text-black transition-opacity ease-in-out`,
+  `pointer-events-none absolute z-10 rounded-lg border border-gray-300 bg-white px-3 py-2 text-center text-sm text-black transition-opacity ease-in-out duration-300`,
   {
     variants: {
       isShadowed: {
