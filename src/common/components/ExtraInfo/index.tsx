f9199911d221a50d2187cc84d9bca04a1a53a8da
mibu119 <horang296@gmail.com>
2024-01-01 10:03:18 +0900

    fix: extrainfo에 variants gap 추가

--- src/common/components/ExtraInfo/index.tsx
+++ src/common/components/ExtraInfo/index.tsx
@@ -1,22 +1,33 @@
+import { VariantProps } from 'class-variance-authority';
 import { Children, ComponentProps, ReactNode } from 'react';
+import { ClassNameValue } from 'tailwind-merge';
 
 import { cn } from '~/utils/cn';
 
-export interface TextProps extends ComponentProps<'div'> {
+import { ExtraInfoVariants } from './ExtraInfo.variants';
+
+export interface TextProps
+  extends VariantProps<typeof ExtraInfoVariants>,
+    ComponentProps<'div'> {
   children: ReactNode | ReactNode[];
+  dividerClassName?: ClassNameValue;
 }
 
-const ExtraInfo = ({ className, children }: TextProps) => {
+const ExtraInfo = ({ className, gap = 1, children }: TextProps) => {
   const childComponents = Children.toArray(children);
 
   return (
-    <ul className={cn('flex text-sm font-thin text-gray-300', className)}>
+    <ul
+      className={cn(
+        'flex text-sm font-thin text-gray-300',
+        ExtraInfoVariants({ gap }),
+        className,
+      )}
+    >
       {childComponents.map((child, index) => (
-        <li key={index}>
+        <li key={index} className={cn('flex', ExtraInfoVariants({ gap }))}>
           {child}
-          {index < childComponents.length - 1 && (
-            <span className="mx-1">·</span>
-          )}
+          {index < childComponents.length - 1 && <span>·</span>}
         </li>
       ))}
     </ul>
--- src/stories/ExtraInfo.stories.tsx
+++ src/stories/ExtraInfo.stories.tsx
@@ -5,13 +5,19 @@ import ExtraInfo from '~/common/components/ExtraInfo';
 const meta = {
   title: 'Common/Components/ExtraInfo',
   component: ExtraInfo,
+  argTypes: {
+    gap: { control: { type: 'range', min: 0, max: 10, step: 1 } },
+  },
+  args: {
+    gap: 1,
+  },
 } satisfies Meta<typeof ExtraInfo>;
 
 export default meta;
 
 export const Default: StoryObj<typeof ExtraInfo> = {
-  render: () => (
-    <ExtraInfo>
+  render: args => (
+    <ExtraInfo {...args}>
       <span>넷플릭스</span>
       <span>19시간 전</span>
     </ExtraInfo>
@@ -19,8 +25,8 @@ export const Default: StoryObj<typeof ExtraInfo> = {
 };
 
 export const Array: StoryObj<typeof ExtraInfo> = {
-  render: () => (
-    <ExtraInfo>
+  render: args => (
+    <ExtraInfo {...args}>
       <span>게시물 10</span>
       <span>팔로워 10</span>
       <span>팔로잉 10</span>
