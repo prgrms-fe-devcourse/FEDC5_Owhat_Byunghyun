d903ccfd0ec921791da85c79dc6374090b9b67a9
mibu <114740795+mibu119@users.noreply.github.com>
2024-01-02 14:51:00 +0900

    feat: extrainfo 컴포넌트 구현 (#47)
    * feat: extrainfo 컴포넌트 구현
    
    * fix: extrainfo에 variants gap 추가
    
    * fix: extrainfo variants 케이스 수정
    
    * fix: 사용안하는 타입 제거, 디폴트 스타일 cva로 이동

---
+++ src/common/components/ExtraInfo/ExtraInfo.variants.ts
@@ -0,0 +1,22 @@
+import { cva } from 'class-variance-authority';
+
+export const extraInfoVariants = cva(`flex text-sm font-thin text-gray-300`, {
+  variants: {
+    gap: {
+      0: `gap-0`,
+      1: `gap-1`,
+      2: `gap-2`,
+      3: `gap-3`,
+      4: `gap-4`,
+      5: `gap-5`,
+      6: `gap-6`,
+      7: `gap-7`,
+      8: `gap-8`,
+      9: `gap-9`,
+      10: `gap-10`,
+    },
+  },
+  defaultVariants: {
+    gap: 1,
+  },
+});
---
+++ src/stories/ExtraInfo.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ExtraInfo from '~/common/components/ExtraInfo';
+
+const meta = {
+  title: 'Common/Components/ExtraInfo',
+  component: ExtraInfo,
+  argTypes: {
+    gap: { control: { type: 'range', min: 0, max: 10, step: 1 } },
+  },
+  args: {
+    gap: 1,
+  },
+} satisfies Meta<typeof ExtraInfo>;
+
+export default meta;
+
+export const Default: StoryObj<typeof ExtraInfo> = {
+  render: args => (
+    <ExtraInfo {...args}>
+      <span>넷플릭스</span>
+      <span>19시간 전</span>
+    </ExtraInfo>
+  ),
+};
+
+export const Array: StoryObj<typeof ExtraInfo> = {
+  render: args => (
+    <ExtraInfo {...args}>
+      <span>게시물 10</span>
+      <span>팔로워 10</span>
+      <span>팔로잉 10</span>
+    </ExtraInfo>
+  ),
+};
