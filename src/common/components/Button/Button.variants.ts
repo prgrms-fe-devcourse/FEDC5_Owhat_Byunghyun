5828c085e85216d795da72fd1e6178c7cab33122
신준혁 <oridori2705@naver.com>
2024-01-03 10:54:51 +0900

    refactor: transtion과 color 변경

--- src/common/components/Button/Button.variants.ts
+++ src/common/components/Button/Button.variants.ts
@@ -1,17 +1,17 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVarients = cva(
-  `relative px-4 py-2 text-md font-semibold transition-opacity`,
+  `text-md relative px-4 py-2 font-semibold transition-colors`,
   {
     variants: {
       styleType: {
         primary:
-          'rounded-lg bg-primary text-white hover:opacity-60 focus:outline-none',
+          'rounded-lg bg-primary text-white hover:bg-[#cc6600] focus:outline-none',
         secondary:
-          'rounded-md bg-white text-primary hover:opacity-60 focus:outline-none',
+          'rounded-md bg-white text-primary hover:bg-[#cc6600] focus:outline-none',
         outline:
-          'rounded-md border border-primary text-primary hover:opacity-60 focus:outline-none',
-        ghost: 'rounded-md text-primary hover:opacity-60 focus:outline-none',
+          'rounded-md border border-primary text-primary hover:bg-[#cc6600] focus:outline-none',
+        ghost: 'hover:bg-[#cc6600]focus:outline-none rounded-md text-primary',
       },
     },
     defaultVariants: {
