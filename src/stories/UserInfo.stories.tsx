01d728be5cdb8e9014860aeb7d1c16707ec7da77
mibu119 <horang296@gmail.com>
2024-01-04 11:03:54 +0900

    refactor: userinfo 스토리북 리팩토링

--- src/stories/UserInfo.stories.tsx
+++ src/stories/UserInfo.stories.tsx
@@ -6,31 +6,36 @@ import UserInfo from '~/common/components/UserInfo';
 const meta = {
   title: 'Common/Components/UserInfo',
   component: UserInfo,
+  argTypes: {
+    _id: { control: 'text' },
+    author: { control: 'text' },
+    channel: { control: 'text' },
+  },
+  args: {
+    createdAt: '2021-10-15T20:48:19.816Z',
+  },
+  decorators: [
+    Story => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 } as Meta<typeof UserInfo>;
 
 export default meta;
 
 export const Default: StoryObj<typeof UserInfo> = {
-  render: () => (
-    <MemoryRouter>
-      <UserInfo
-        _id="12345"
-        author="고양이"
-        channel="넷플릭스"
-        createdAt="2021-10-15T20:48:19.816Z"
-      />
-    </MemoryRouter>
-  ),
+  args: {
+    _id: '6169b2d3d5f8f3e7e8e5f5f5',
+    author: '홍길동',
+    channel: '넷플릭스',
+  },
 };
 
 export const Comment: StoryObj<typeof UserInfo> = {
-  render: () => (
-    <MemoryRouter>
-      <UserInfo
-        _id="12345"
-        author="김철수"
-        createdAt="2021-10-15T20:48:19.816Z"
-      />
-    </MemoryRouter>
-  ),
+  args: {
+    _id: '6169b2d3d5f8f3e7e8e5f5f5',
+    author: '홍길동',
+  },
 };
