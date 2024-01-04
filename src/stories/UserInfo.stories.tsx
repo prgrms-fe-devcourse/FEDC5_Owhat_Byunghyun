cda91531008bbe028e06aee525c5f301ce539b85
mibu119 <horang296@gmail.com>
2024-01-04 10:58:21 +0900

    fix: userinfo 스토리북 에러 해결

--- src/stories/UserInfo.stories.tsx
+++ src/stories/UserInfo.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import UserInfo from '~/common/components/UserInfo';
 
@@ -11,21 +12,25 @@ export default meta;
 
 export const Default: StoryObj<typeof UserInfo> = {
   render: () => (
-    <UserInfo
-      _id="12345"
-      author="고양이"
-      channel="넷플릭스"
-      createdAt="2021-10-15T20:48:19.816Z"
-    />
+    <MemoryRouter>
+      <UserInfo
+        _id="12345"
+        author="고양이"
+        channel="넷플릭스"
+        createdAt="2021-10-15T20:48:19.816Z"
+      />
+    </MemoryRouter>
   ),
 };
 
 export const Comment: StoryObj<typeof UserInfo> = {
   render: () => (
-    <UserInfo
-      _id="12345"
-      author="김철수"
-      createdAt="2021-10-15T20:48:19.816Z"
-    />
+    <MemoryRouter>
+      <UserInfo
+        _id="12345"
+        author="김철수"
+        createdAt="2021-10-15T20:48:19.816Z"
+      />
+    </MemoryRouter>
   ),
 };
