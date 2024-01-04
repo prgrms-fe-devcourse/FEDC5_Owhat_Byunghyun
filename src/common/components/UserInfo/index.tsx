d54446179eddcd8d3f4591ac1c273c99143986b3
mibu119 <horang296@gmail.com>
2024-01-04 10:11:30 +0900

    feat: userinfo 컴포넌트 구현, 시간 계산 유틸 함수 생성

---
+++ src/common/components/UserInfo/index.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+
+import { getElapsedTime } from '~/utils/getElaspedTime';
+
+import Avatar from '../Avatar';
+import ExtraInfo from '../ExtraInfo';
+import Group from '../Group';
+import Text from '../Text';
+
+interface UserInfoProps {
+  _id: string;
+  profileImage?: string;
+  author: string;
+  channel?: string;
+  createdAt: string;
+}
+
+const UserInfo = ({
+  _id,
+  profileImage,
+  author,
+  channel,
+  createdAt,
+}: UserInfoProps) => {
+  return (
+    <>
+      <Group spacing={'md'} align={'center'}>
+        <Link to={`/profile/${_id}`}>
+          <Avatar src={profileImage}></Avatar>
+        </Link>
+        <div>
+          <Link to={`/profile/${_id}`}>
+            <Text id={_id} strong>
+              {author}
+            </Text>
+          </Link>
+          <ExtraInfo>
+            {channel ? <span>{channel}</span> : null}
+            <span>{getElapsedTime(createdAt)}</span>
+          </ExtraInfo>
+        </div>
+      </Group>
+    </>
+  );
+};
+
+export default UserInfo;
