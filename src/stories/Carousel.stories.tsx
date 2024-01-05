db28e137c7da4d5665fed18c7fb57afc4fe82c54
신준혁 <oridori2705@naver.com>
2024-01-05 09:19:13 +0900

    refactor: Carousel 이름 변경

--- src/stories/Carousel.stories.tsx
+++ src/stories/Carousel.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import Carousel from '~/common/components/Carousel';
 
 export default {
-  title: 'Common/Components/Slide',
+  title: 'Common/Components/Carousel',
   component: Carousel,
   argTypes: {
     itemsToShow: {
@@ -35,7 +35,12 @@ export const Default: Story = {
       <Carousel className="border-y-2" {...args}>
         {Array.from(new Array(20), (_, k) => k).map(i => (
           <div className="relative inline-block overflow-hidden rounded-full bg-gray-200 ring-1 ring-gray-400 ring-offset-2">
-            <img key={i} alt="아바타" src={`https://picsum.photos/300?${i}`} />
+            <img
+              key={i}
+              alt="아바타"
+              draggable="false"
+              src={`https://picsum.photos/300?${i}`}
+            />
           </div>
         ))}
       </Carousel>
