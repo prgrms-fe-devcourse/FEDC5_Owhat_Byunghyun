278361a397baa031ab41e4eb75295cc67d274fab
신준혁 <90139306+oridori2705@users.noreply.github.com>
2024-01-05 15:22:29 +0900

    feat: Badge 컴포넌트 구현 (#65)
    * feat: Badge 컴포넌트 구현
    
    * refactor: animate 오타 수정
    
    * refactor: 자식선택자 사용으로 리팩토링 및 absoulte 위치 자동화
    
    * refactor: 온라인 뱃지 색상 변경
    
    * refactor: badge 위치 children요소에 맞춰지도록 수정

---
+++ src/stories/Badge.stories.tsx
@@ -0,0 +1,61 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Avatar from '~/common/components/Avatar';
+import Badge from '~/common/components/Badge';
+import Icon from '~/common/components/Icon';
+export default {
+  title: 'Common/Components/Badge',
+  component: Badge,
+  argTypes: {
+    badgeType: {
+      options: ['online', 'offline', 'alarm'],
+      control: 'inline-radio',
+    },
+    corner: {
+      options: ['top-right', 'top-left', 'bottom-right', 'bottom-left'],
+      control: 'inline-radio',
+    },
+    size: {
+      options: ['xsmall', 'small', 'medium', 'large', 'xlarge'],
+      control: 'inline-radio',
+    },
+  },
+  args: {
+    badgeType: 'alarm',
+    corner: 'top-right',
+  },
+} satisfies Meta<typeof Badge>;
+
+type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {
+  render: function Render(args) {
+    return (
+      <Badge {...args}>
+        <Avatar size="small" src="http://picsum.photos/200"></Avatar>
+      </Badge>
+    );
+  },
+};
+
+export const BigElement: Story = {
+  render: function Render(args) {
+    return (
+      <Badge {...args}>
+        <Avatar size="full" src="http://picsum.photos/200"></Avatar>
+      </Badge>
+    );
+  },
+};
+
+export const IconBadge: Story = {
+  render: function Render(args) {
+    return (
+      <Badge {...args}>
+        <div>
+          <Icon id="notifications"></Icon>
+        </div>
+      </Badge>
+    );
+  },
+};
