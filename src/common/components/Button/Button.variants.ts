65c78b39e02bd1b729df871ca261a07ee8e0b4c2
신준혁 <90139306+oridori2705@users.noreply.github.com>
2024-01-05 14:23:39 +0900

    refactor: button fullWidth 기능 수정 및 스타일 수정 (#83)

--- src/common/components/Button/Button.variants.ts
+++ src/common/components/Button/Button.variants.ts
@@ -1,18 +1,18 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVarients = cva(
-  'group relative flex p-small text-base font-semibold transition-colors',
+  'relative flex justify-center border p-small text-base font-semibold transition-colors',
   {
     variants: {
       styleType: {
         primary:
-          'rounded-small bg-primary fill-white text-white hover:bg-primary-darker focus:outline-none',
+          'rounded-small  border-transparent bg-primary fill-white text-white hover:bg-primary-darker focus:outline-none',
         secondary:
-          'rounded-small bg-white fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white focus:outline-none',
+          'rounded-small  border-transparent  bg-white fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white focus:outline-none',
         outline:
-          'focus:outline-nonefill-primary rounded-small border border-primary fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white ',
+          'focus:outline-nonefill-primary box-border rounded-small border border-primary fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white ',
         ghost:
-          'rounded-small text-primary hover:fill-gray-600 hover:fill-gray-600 hover:text-primary-darker focus:outline-none',
+          'rounded-small border-transparent  text-primary hover:fill-gray-600 hover:fill-gray-600 hover:text-primary-darker focus:outline-none',
       },
       disabled: {
         true: 'pointer-events-none border-gray-300 bg-gray-400 fill-white',
