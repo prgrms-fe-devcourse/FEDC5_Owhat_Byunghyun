00bea1135eb10910265d70e03ae3edeacd7e1858
신준혁 <oridori2705@naver.com>
2024-01-09 22:29:48 +0900

    refactor: recat Query 사용으로 인한 storybook 수정

--- src/stories/RegisterEmailInput.stories.tsx
+++ src/stories/RegisterEmailInput.stories.tsx
@@ -1,12 +1,29 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import EmailInput from '~/pages/register/components/EmailInput';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 5,
+      retry: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default {
   title: 'Common/Components/EmailInput',
   component: EmailInput,
   argTypes: {},
   args: {},
+  decorators: Story => (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  ),
 } satisfies Meta<typeof EmailInput>;
 
 type Story = StoryObj<typeof EmailInput>;
