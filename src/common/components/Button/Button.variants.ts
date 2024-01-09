7d5e1c4be455b5e00275da092e83388e6794fded
신준혁 <oridori2705@naver.com>
2024-01-09 11:48:39 +0900

    refactor: 스타일 코드 오타 수정

--- src/common/components/Button/Button.variants.ts
+++ src/common/components/Button/Button.variants.ts
@@ -6,13 +6,13 @@ export const buttonVarients = cva(
     variants: {
       styleType: {
         primary:
-          'rounded-small border-transparent bg-primary fill-white text-white hover:bg-primary-darker focus:outline-none disabled:pointer-events-none disabled:bg-primary-lighter',
+          'rounded-small border-transparent bg-primary fill-white text-white hover:bg-primary-darker disabled:pointer-events-none disabled:bg-primary-lighter',
         secondary:
-          'focus:outline- rounded-small border-transparent bg-white fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white disabled:pointer-events-none disabled:fill-primary-lighter disabled:text-primary-lighter',
+          'rounded-small border-transparent bg-white fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white disabled:pointer-events-none disabled:fill-primary-lighter disabled:text-primary-lighter',
         outline:
-          'focus:outline-nonefill-primary box-border rounded-small border border-primary fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white disabled:pointer-events-none disabled:border-primary-lighter disabled:fill-primary-lighter disabled:text-primary-lighter',
+          'box-border rounded-small border border-primary fill-primary fill-primary text-primary hover:bg-primary hover:fill-white hover:text-white disabled:pointer-events-none disabled:border-primary-lighter disabled:fill-primary-lighter disabled:text-primary-lighter',
         ghost:
-          'rounded-small border-transparent text-primary hover:fill-gray-600 hover:fill-gray-600 hover:text-primary-darker focus:outline-none disabled:pointer-events-none disabled:fill-gray-500 disabled:text-primary-lighter',
+          'rounded-small border-transparent text-primary hover:fill-gray-600 hover:fill-gray-600 hover:text-primary-darker disabled:pointer-events-none disabled:fill-gray-500 disabled:text-primary-lighter',
       },
     },
     defaultVariants: {
