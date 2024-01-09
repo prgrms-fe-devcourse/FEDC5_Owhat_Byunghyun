186dde6195266597eb2a54457595cf28f0c7cede
신준혁 <oridori2705@naver.com>
2024-01-09 15:29:13 +0900

    refactor: storybook의 queryClient 문제 해결

--- src/stories/RegisterForm.stories.tsx
+++ src/stories/RegisterForm.stories.tsx
@@ -1,12 +1,29 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import RegisterForm from '~/pages/register/components/RegisterForm';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 5,
+      retry: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default {
   title: 'Common/Components/RegisterForm',
   component: RegisterForm,
   argTypes: {},
   args: {},
+  decorators: Story => (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  ),
 } satisfies Meta<typeof RegisterForm>;
 
 type Story = StoryObj<typeof RegisterForm>;
