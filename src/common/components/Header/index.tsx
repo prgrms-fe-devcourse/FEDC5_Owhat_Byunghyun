22e5f30de1071e0ee434f1663e1dff17c8e0d67b
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2024-01-09 11:18:48 +0900

    feat: Page Layout 컴포넌트 생성 (#75)
    * feat: Header 컴포넌트 생성
    
    * feat: Page Layout 컴포넌트 생성
    
    * feat: useLayout hooks 추가
    
    * fix: group 컴포넌트 variants 속성 오타 제거
    
    * refactor: Header, Layout 수정
    
    style: logo element type 변경
    
    style: Header 양쪽 요소 크기 제한
    
    refactor: 기본 document title 수정
    
    test: Layout story 추가
    
    * refactor: header layout을 구조화
    
    style: header layout 수정
    
    chore: 불필요한 유틸 타입 제거
    
    chore: cursor option 변경

---
+++ src/common/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+
+import Logo from '../Logo';
+
+export interface HeaderProps {
+  title?: ReactNode;
+  left: ReactNode;
+  right: ReactNode;
+}
+
+const Header = ({ title, left, right }: HeaderProps) => {
+  return (
+    <header className="mb grid w-full grid-cols-[1fr,4fr,1fr] items-center border-b px-small py text-center">
+      <span className="justify-self-start">{left}</span>
+      {title ? <b>{title}</b> : <Logo />}
+      <span className="justify-self-end">{right}</span>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/common/components/Layout/index.tsx
@@ -0,0 +1,49 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+import Header, { type HeaderProps } from '../Header';
+
+interface ILayoutContext extends HeaderProps {
+  changeMeta: ({ title, left, right }: HeaderProps) => void;
+}
+
+interface LayoutProviderProps {
+  children: ReactNode;
+}
+
+export const LayoutContext = createContext<ILayoutContext>(
+  {} as ILayoutContext,
+);
+
+const LayoutProvider = ({ children }: LayoutProviderProps) => {
+  const [template, setTemplate] = useState<HeaderProps>({
+    title: '',
+    left: null,
+    right: null,
+  });
+
+  const changeMeta = ({ title, left, right }: HeaderProps) => {
+    setTemplate({
+      ...template,
+      title: title || template.title,
+      left: left || template.left,
+      right: right || template.right,
+    });
+  };
+
+  useEffect(() => {
+    document.title = `${
+      template.title || 'Owhat!'
+    } | OTT에 대한 정보를 한 번에!`;
+  }, [template.title]);
+
+  return (
+    <LayoutContext.Provider value={{ ...template, changeMeta }}>
+      <main className="max-w-layout px">
+        <Header {...template} />
+        {children}
+      </main>
+    </LayoutContext.Provider>
+  );
+};
+
+export default LayoutProvider;
