c0f5fae54cfcec3efa4d66bb1678f8d38d9639c3
hy219 <jhy_0219@daum.net>
2024-01-10 04:42:11 +0900

    feat: 경로 활성 시 해당 Icon에 css 적용 및 map()으로 중복코드 최소화
    NavLink 컴포넌트의 isActive 속성을 사용하여, 올바른 경로 활성 시 css 적용

--- src/common/components/NavigationBar/NavigationBar.variants.tsx
+++
@@ -1,5 +0,0 @@
-import { cva } from 'class-variance-authority';
-
-export const NavigationBarVariants = cva(
-  `rounded-full p-1 hover:bg-primary focus:bg-primary`,
-);
--- src/common/components/NavigationBar/index.tsx
+++ src/common/components/NavigationBar/index.tsx
@@ -1,42 +1,50 @@
-import { VariantProps } from 'class-variance-authority';
 import { ComponentProps } from 'react';
-import { Link } from 'react-router-dom';
-
-import { cn } from '~/utils/cn';
+import { NavLink } from 'react-router-dom';
 
 import Group from '../Group';
 import Icon from '../Icon';
-import { NavigationBarVariants } from './NavigationBar.variants';
+import { IconName } from '../Icon/type';
 
-export interface NavigationBarProps
-  extends VariantProps<typeof NavigationBarVariants>,
-    ComponentProps<'div'> {}
+export interface NavigationBarProps extends ComponentProps<'nav'> {}
 
-const NavigationBar = ({ ...props }: NavigationBarProps) => {
+interface NavLinkInfo {
+  to: string;
+  id: IconName;
+  label: string;
+}
+
+const navLinks: NavLinkInfo[] = [
+  { to: '/', id: 'home', label: '메인' },
+  { to: '/search', id: 'search', label: '검색' },
+  { to: '/message', id: 'message', label: '메세지' },
+  { to: '/notification', id: 'notifications', label: '알림' },
+  { to: '/account', id: 'account-circle', label: '유저 정보' },
+];
+
+const NavigationBar = () => {
   return (
-    <Group
-      position="evenly"
-      align="center"
-      spacing="md"
-      className="absolute bottom-0 h-14 w-full rounded border-2 border-primary-lighter bg-primary-lighter"
-      {...props}
-    >
-      <Link to={'/'} className={cn(NavigationBarVariants())}>
-        <Icon id="home" aria-label="메인" />
-      </Link>
-      <Link to={'/search'} className={cn(NavigationBarVariants())}>
-        <Icon id="search" aria-label="검색" />
-      </Link>
-      <Link to={'/message'} className={cn(NavigationBarVariants())}>
-        <Icon id="message" aria-label="메세지" />
-      </Link>
-      <Link to={'/notification'} className={cn(NavigationBarVariants())}>
-        <Icon id="notifications" aria-label="알림" />
-      </Link>
-      <Link to={'/account'} className={cn(NavigationBarVariants())}>
-        <Icon id="account-circle" aria-label="유저 정보" />
-      </Link>
-    </Group>
+    <nav>
+      <Group
+        position="evenly"
+        align="center"
+        spacing="md"
+        className="absolute bottom-0 h-14 w-full rounded border-2 border-primary-lighter bg-primary-lighter"
+      >
+        {navLinks.map(({ to, id, label }) => (
+          <NavLink
+            key={id}
+            to={to}
+            className={({ isActive }) =>
+              isActive
+                ? 'rounded-full bg-primary p-1'
+                : 'rounded-full p-1 hover:bg-primary'
+            }
+          >
+            <Icon id={id} aria-label={label} />
+          </NavLink>
+        ))}
+      </Group>
+    </nav>
   );
 };
 
