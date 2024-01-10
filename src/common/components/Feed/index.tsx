ae5fb5c153731aa60162fbca27cf3638897831e7
Hyeyeon Jeong <jhy_0219@daum.net>
2024-01-10 10:47:58 +0900

    Feed 컴포넌트 구현 (#84)
    * style: Feed 컴포넌트 디자인 구현
    
    * feat: Post DUMMY DATA로 데이터 불러와서 출력
    
    * refactor: 좋아요 아이콘을 토글 버튼으로 변경 및 초기값 설정
    
    초기값을 지정하지 않거나, true로 지정할 경우 -> 채워진 좋아요 버튼이 먼저 화면에 출력됨
    
    * feat: Post DUMMY_DATA를 초기값으로 설정 및 type 지정
    
    외부 컴포넌트에서 데이터를 주입할 수 있도록 함
    
    * refactor: Post DUMMY_DATA 삭제
    
    데이터 초기값을 지정해주지 않더라도 오류 발생 x
    
    * docs: DUMMY_DATA.json 파일 삭제
    
    * feat: Feed 컴포넌트 storybook 추가
    
    * refactor: storybook argType options를 array로 변경
    
    title, image, body의 options 변경
    
    * refactor: initialState를 props로 주입
    
    * refactor: threshold를 0으로 설정해 이미지 출력 속도 향상
    
    lazy기능의 설정
    
    * refactor: title을 필수 속성으로 변경
    
    optional 제거
    
    * feat: image가 없을 경우 null 반환
    
    image type으로 optional을 줄 경우, Image 컴포넌트에서 string으로 type을 지정해놨기 때문에 error 발생
    
    * refactor: 논리 연산자(&&)로 이미지가 있을 경우만 이미지 출력 처리
    
    삼항연산자에서 논리 연산자로 변경하여 처리해 더 간편한 코드 작성
    
    * refactor: likes, comments의 type string[]으로 변경
    
    * feat: imgAspect가 true일 경우, 이미지 비율 16:9로 제한
    
    'aspect-video'속성을 이용하여, imgAspect가 true일 경우 16:9로 이미지 크기를 제한한다.
    
    * refactor: body의 optional type 제거
    
    글도 필수로 작성되어야하는 항목으로 둠
    
    * feat: Like 버튼 클릭 시 initialState값 true로 변경 & 재클릭 시 false로 변경
    
    좋아요 버튼 클릭 시, 클릭 한 것을 알 수 있도록, initialState를 true로 변경 & 재클릭시 false로 변경
    
    * feat: title과 body의 글자 수가 많을 때, 말줄임표로 처리
    
    title 은 한 줄 이상일 때, body는 두 줄 이상일 때 -> 말줄임표로 처리해 줌(inline-clamsp-)
    
    * feat: Like에 onClick 이벤트 핸들러 추가
    
    props로 동작을 주입받을 수 있도록 함
    
    * feat: 좋아요와 댓글 수가 100개 이상일 때, 99+로 표시
    
    array.length는 배열의 요소를 탐색하지 않고 길이를 구하므로, 배열의 길이로 인한 계산 속도를 고려할 필요가 없다
    
    * refactor: 댓글 아이콘의 설명 속성 추가
    
    * refactor: storybook 좋아요, 댓글 수에 따른 text 변화 확인
    
    likes에도 적용 - 0개, 5개, 100개
    
    * fix: 좋아요와 댓글 레이아웃 붕괴 현상 해결
    
    좋아요 수가 많아져 text의 길이가 늘어날 경우, 댓글 아이콘과 text가 뒤로 밀리는 현상 해결
    
    * style: 좋아요와 댓글의 간격을 줄이고, text 가운데 정렬
    
    정의된 값 이외의 값을 유동적으로 지정해 줌

---
+++ src/common/components/Feed/index.tsx
@@ -0,0 +1,67 @@
+import Group from '../Group';
+import Icon from '../Icon';
+import Image from '../Image';
+import Like from '../Like';
+import Text from '../Text';
+
+interface FeedProps {
+  initialState: boolean;
+  title: string;
+  image: string;
+  body: string;
+  likes: string[];
+  comments: string[];
+  imgAspect?: boolean;
+  textOverflow?: boolean;
+  handleLike?: () => void;
+}
+
+const Feed = ({
+  initialState = false,
+  title,
+  image,
+  body,
+  likes,
+  comments,
+  imgAspect = true,
+  textOverflow = true,
+  handleLike,
+}: FeedProps) => {
+  const fullImage = imgAspect ? 'aspect-video' : '';
+  const abbreviationTitle = textOverflow ? 'line-clamp-1' : '';
+  const abbreviationBody = textOverflow ? 'line-clamp-2' : '';
+
+  const likeLength = likes.length > 99 ? '99+' : likes.length;
+  const commentsLength = comments.length > 99 ? '99+' : comments.length;
+
+  return (
+    <Group direction="columns" spacing="md">
+      <Text size="xlarge" strong={true} className={abbreviationTitle}>
+        {title}
+      </Text>
+      {image && (
+        <Image
+          src={image}
+          imgWidth="full"
+          imgHeight="auto"
+          lazy={true}
+          threshold={0}
+          className={fullImage}
+        />
+      )}
+      <Text className={abbreviationBody}>{body}</Text>
+      <Group spacing={4} align="center">
+        <Like initialState={initialState} onClick={handleLike} />
+        <Text size="small" className="w-6 text-center">
+          {likeLength}
+        </Text>
+        <Icon id="sms" aria-label="댓글 아이콘" />
+        <Text size="small" className="w-6 text-center">
+          {commentsLength}
+        </Text>
+      </Group>
+    </Group>
+  );
+};
+
+export default Feed;
