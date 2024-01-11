cf463f73ad445e254641e274875218abc9b0e1db
mibu119 <horang296@gmail.com>
2024-01-11 22:11:09 +0900

    refactor: isEdit 분기 제거

--- src/pages/account/components/AccountImages/index.tsx
+++ src/pages/account/components/AccountImages/index.tsx
@@ -3,19 +3,13 @@ import { createElement } from 'react';
 import Avatar from '~/common/components/Avatar';
 import Group from '~/common/components/Group';
 import Image from '~/common/components/Image';
-import Upload from '~/common/components/Upload';
 
 interface AccountImagesProps {
   coverImage?: string;
   image?: string;
-  isEdit?: boolean;
 }
 
-const AccountImages = ({
-  coverImage,
-  image,
-  isEdit = false,
-}: AccountImagesProps) => {
+const AccountImages = ({ coverImage, image }: AccountImagesProps) => {
   const defaultCoverImage = createElement('div', {
     className: 'inline-block min-h-[150px] min-w-layout bg-transparent',
   });
@@ -27,24 +21,7 @@ const AccountImages = ({
       position={'center'}
       className="relative"
     >
-      {isEdit ? (
-        <Upload id="coverImage" name="coverImage">
-          {src =>
-            src ? (
-              <div className="flex w-full items-center justify-center">
-                <Image
-                  imgHeight={'full'}
-                  imgWidth={'full'}
-                  src={src}
-                  className="inline-block aspect-[2/1] min-h-[150px]"
-                />
-              </div>
-            ) : (
-              defaultCoverImage
-            )
-          }
-        </Upload>
-      ) : coverImage ? (
+      {coverImage ? (
         <div className="flex w-full items-center justify-center">
           <Image
             imgHeight={'full'}
@@ -57,13 +34,7 @@ const AccountImages = ({
         defaultCoverImage
       )}
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-1/4">
-        {isEdit ? (
-          <Upload id="profileImage" name="profileImage">
-            {src => <Avatar src={src || image} size="small" />}
-          </Upload>
-        ) : (
-          <Avatar src={image} size="small" />
-        )}
+        <Avatar src={image} size="small" />
       </div>
     </Group>
   );
