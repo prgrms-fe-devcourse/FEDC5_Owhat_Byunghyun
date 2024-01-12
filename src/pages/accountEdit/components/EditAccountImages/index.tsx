b5cc77134c51be69d863ecb43139356969878318
mibu119 <horang296@gmail.com>
2024-01-12 09:28:04 +0900

    fix: upload onchange 사용으로 변경

--- src/pages/accountEdit/components/EditAccountImages/index.tsx
+++ src/pages/accountEdit/components/EditAccountImages/index.tsx
@@ -24,46 +24,44 @@ const EditAccountImages = ({
       position={'center'}
       className="relative"
     >
-      <Upload id="coverImage" name="coverImage">
-        {(src, file) => {
-          if (file) {
-            setCoverImageFile(file);
-          }
-
-          return (
-            <div className="flex w-full items-center justify-center">
-              {src ? (
-                <Image
-                  imgHeight={'full'}
-                  imgWidth={'full'}
-                  src={src}
-                  className="inline-block aspect-[2/1] min-h-[150px]"
-                />
-              ) : coverImage ? (
-                <Image
-                  imgHeight={'full'}
-                  imgWidth={'full'}
-                  src={coverImage}
-                  className="inline-block aspect-[2/1] min-h-[150px]"
-                />
-              ) : (
-                <div className="inline-block min-h-[150px] min-w-layout bg-transparent">
-                  <button className="h-full w-full"></button>
-                </div>
-              )}
-            </div>
-          );
+      <Upload
+        id="coverImage"
+        name="coverImage"
+        onChange={file => {
+          setCoverImageFile(file);
         }}
+      >
+        {src => (
+          <div className="flex w-full items-center justify-center">
+            {src ? (
+              <Image
+                imgHeight={'full'}
+                imgWidth={'full'}
+                src={src}
+                className="inline-block aspect-[2/1] min-h-[150px]"
+              />
+            ) : coverImage ? (
+              <Image
+                imgHeight={'full'}
+                imgWidth={'full'}
+                src={coverImage}
+                className="inline-block aspect-[2/1] min-h-[150px]"
+              />
+            ) : (
+              <div className="inline-block min-h-[150px] min-w-layout bg-transparent">
+                <button className="h-full w-full"></button>
+              </div>
+            )}
+          </div>
+        )}
       </Upload>
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-1/4">
-        <Upload id="profileImage" name="profileImage">
-          {(src, file) => {
-            if (file) {
-              setImageFile(file);
-            }
-
-            return <Avatar src={src || image} size="small" />;
-          }}
+        <Upload
+          id="profileImage"
+          name="profileImage"
+          onChange={file => setImageFile(file)}
+        >
+          {src => <Avatar src={src || image} size="small" />}
         </Upload>
       </div>
     </Group>
