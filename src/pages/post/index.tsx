e708a6dbd48b06ef058be0062646a477e5e13b77
hy219 <jhy_0219@daum.net>
2024-01-15 07:12:07 +0900

    feat: PostCreatePage 경로 생성

---
+++ src/pages/post/index.tsx
@@ -0,0 +1,11 @@
+import Icon from '~/common/components/Icon';
+import Text from '~/common/components/Text';
+
+export default function PostCreatePage() {
+  return (
+    <section>
+      <Text>이미지 추가</Text>
+      <Icon id="add-photo" />
+    </section>
+  );
+}
