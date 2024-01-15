13cb0e2961fd352c76a50067b61b7412aabc05cf
mibu119 <horang296@gmail.com>
2024-01-16 00:33:05 +0900

    feat: followlist 구현
    fix: accountpage 권한분기 로직 수정 / userposts에 feeditem 추가

--- src/pages/account/components/PostsList/index.tsx
+++ src/pages/account/components/PostsList/index.tsx
@@ -1,58 +1,27 @@
-import Feed from '~/common/components/Feed';
 import Group from '~/common/components/Group';
-import UserInfo from '~/common/components/UserInfo';
 import { UserPost } from '~/common/hooks/queries/useUserPosts';
+import FeedItem from '~/pages/home/components/FeedItem';
 
 interface PostsListProps {
   userPosts: UserPost[];
   isMyAccount: boolean;
 }
 
-const PostsList = ({ userPosts = [], isMyAccount }: PostsListProps) => {
+const PostsList = ({ userPosts = [] }: PostsListProps) => {
   return (
     <ul className="mt flex flex-col border-t border-t-primary-lighter pt">
-      {userPosts.map(
-        ({
-          _id,
-          author,
-          channel,
-          title,
-          createdAt,
-          image,
-          likes,
-          comments,
-          content,
-        }) => {
-          const { _id: userId, image: profileImage, fullName } = author;
-          const { name: channelName } = channel;
-
-          return (
-            <Group
-              key={_id}
-              spacing={'md'}
-              direction={'columns'}
-              className="w-full  p"
-            >
-              <UserInfo
-                _id={userId}
-                profileImage={profileImage}
-                author={fullName}
-                channel={channelName}
-                createdAt={createdAt}
-                isMyAccount={isMyAccount}
-              />
-              <Feed
-                content={content}
-                initialState={false}
-                title={title}
-                image={image}
-                likes={likes}
-                comments={comments}
-              />
-            </Group>
-          );
-        },
-      )}
+      {userPosts.map(feed => {
+        return (
+          <Group
+            key={feed._id}
+            spacing={'md'}
+            direction={'columns'}
+            className="w-full p"
+          >
+            <FeedItem feed={feed} />
+          </Group>
+        );
+      })}
     </ul>
   );
 };
