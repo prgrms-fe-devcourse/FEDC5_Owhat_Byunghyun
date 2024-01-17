d9ebf6096ce23afe31535d8c3ff1439e8453c3ea
mibu119 <horang296@gmail.com>
2024-01-17 19:33:59 +0900

    style: morebutton 내 텍스트 가운데 정렬

--- src/pages/postDetail/components/MoreButton/index.tsx
+++ src/pages/postDetail/components/MoreButton/index.tsx
@@ -24,25 +24,29 @@ const MoreButton = ({
         />
       }
       placement="bottom-right"
-      className="pointer-events-auto min-w-[45px]"
+      className="pointer-events-auto"
     >
       {type === 'post' && (
-        <Button
-          onClick={() => handleEdit && handleEdit()}
-          styleType="secondary"
-          className="z-10 p-[3px] text-xs"
-        >
-          수정
-        </Button>
+        <div className="flex w-[35px] flex-col">
+          <Button
+            onClick={() => handleEdit && handleEdit()}
+            styleType="secondary"
+            className="z-10 p-1 text-xs"
+          >
+            수정
+          </Button>
+        </div>
       )}
 
-      <Button
-        onClick={() => handleDelete(id)}
-        styleType="secondary"
-        className="z-10 p-[3px] text-xs text-error hover:bg-error"
-      >
-        삭제
-      </Button>
+      <div className="flex w-[35px] flex-col">
+        <Button
+          onClick={() => handleDelete(id)}
+          styleType="secondary"
+          className="z-10 p-1  text-xs text-error hover:bg-error"
+        >
+          삭제
+        </Button>
+      </div>
     </Tooltip>
   );
 };
